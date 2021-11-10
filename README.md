e7da2811cde20d1b83432a18b1aab4d9ed9216be
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2021-11-10 17:07:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple C HTTP Server
+
+WIP
---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include "httpd.h"
+
+int main(int c, char** v) {
+    serve_forever("1234");
+    return 0;
+}
+
+void route() {
+    ROUTE_START()
+
+    ROUTE_GET("/") {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_POST("/") {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+  
+    ROUTE_END()
+}
